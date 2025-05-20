82aaa547a4aefa173318c71dbfd69ec2c8d348d9
Rami Yassein Eltayeb Mohamed <149052853+Ramimoha1@users.noreply.github.com>
2025-05-20 16:21:26 +0800

    done contact us
    this should be able to send emails

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $student = $_POST['student'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // You can save to a database, or send an email here
+    echo "Message received from $name for $student. We will contact you at $email.";
+} else {
+    echo "Invalid request method.";
+}
+?>
